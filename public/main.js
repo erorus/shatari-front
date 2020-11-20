1e3b61138a1b7768984255eda616adf8475f551f
Erorus <github@everynothing.net>
2020-11-20 13:49:49 -0500

    select categories and subcategories

--- public/main.js
+++ public/main.js
@@ -5,6 +5,9 @@
 
     const my = {
         categories: undefined,
+
+        classId: undefined,
+        subClassId: undefined,
     };
 
     /**
@@ -27,22 +30,81 @@
                 ct(cat.name)
             );
             categoriesParent.appendChild(catDiv);
+            catDiv.addEventListener('click', clickCategory.bind(null, catDiv));
 
             (cat.subcategories || []).forEach(function (subcat) {
                 const subcatDiv = ce(
                     'div',
                     {
-                        className: 'category subcategory',
+                        className: 'subcategory',
                         dataset: {
-                            classId: cat['class'],
-                            subClassId: cat.subClass,
+                            parentClass: cat['class'],
+                            classId: subcat['class'],
+                            subClassId: subcat.subClass,
                         },
                     },
                     ct(subcat.name)
                 );
                 categoriesParent.appendChild(subcatDiv);
+                subcatDiv.addEventListener('click', clickSubCategory.bind(null, subcatDiv));
+
             });
         });
+    }
+
+    /**
+     * Event handler for clicking a primary category.
+     *
+     * @param {HTMLElement} catDiv
+     */
+    function clickCategory(catDiv) {
+        const classId = parseInt(catDiv.dataset.classId);
+        let wasSelected = !!catDiv.dataset.selected;
+
+        // De-select everything.
+        qsa('.main .categories > div').forEach(function (node) {
+            delete node.dataset.selected;
+            delete node.dataset.visible;
+        });
+        my.classId = undefined;
+        my.subClassId = undefined;
+
+        if (!wasSelected) {
+            // Select this category.
+            catDiv.dataset.selected = 1;
+            my.classId = classId;
+
+            // Show any subcategories under this category.
+            qsa('.main .categories .subcategory[data-parent-class="' + classId + '"]').forEach(function (node) {
+                node.dataset.visible = 1;
+            });
+        }
+    }
+
+    /**
+     * Event handler for clicking a subcategory.
+     *
+     * @param {HTMLElement} subCatDiv
+     */
+    function clickSubCategory(subCatDiv) {
+        let wasSelected = !!subCatDiv.dataset.selected;
+
+        // De-select every subcategory.
+        qsa('.main .categories .subcategory[data-selected]').forEach(function (node) {
+            delete node.dataset.selected;
+        });
+
+        if (!wasSelected) {
+            // Select this subcategory.
+            subCatDiv.dataset.selected = 1;
+
+            my.classId = parseInt(subCatDiv.dataset.classId);
+            my.subClassId = parseInt(subCatDiv.dataset.subClassId);
+        } else {
+            // De-select this subcategory, reverting back to the parent category criteria.
+            my.classId = parseInt(subCatDiv.dataset.parentClass);
+            my.subClassId = undefined;
+        }
     }
 
     /**
