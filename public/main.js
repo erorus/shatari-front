98632fcbfaad98f9c5473db27815a1082e186f6a
Erorus <erorus@users.noreply.github.com>
2023-05-07 13:17:05 -0400

    set up migration to undermine.exchange

--- public/main.js
+++ public/main.js
@@ -5871,6 +5871,110 @@ new function () {
         init();
     };
 
+    /**
+     * Handles migrating from oribos.exchange to undermine.exchange.
+     */
+    const UndermineMigration = new function () {
+        // ********************* //
+        // ***** VARIABLES ***** //
+        // ********************* //
+
+        const my = Object.seal({
+            abortInit: false,
+        });
+
+        // ********************* //
+        // ***** FUNCTIONS ***** //
+        // ********************* //
+
+        // ------ //
+        // PUBLIC //
+        // ------ //
+
+        /**
+         * Returns true when the full page init should be aborted (because we're about to redirect).
+         *
+         * @return {boolean}
+         */
+        this.abortInit = () => my.abortInit;
+
+        // ------- //
+        // PRIVATE //
+        // ------- //
+
+        function init() {
+            switch (location.host) {
+                case 'oribos.exchange':
+                    oribosPart();
+                    break;
+
+                case 'undermine.exchange':
+                    underminePart();
+                    break;
+            }
+        }
+
+        /**
+         * Receives all cross-document messages. Listens for those coming from oribos.exchange to receive local storage
+         * migration information.
+         *
+         * @param {MessageEvent} event
+         */
+        function migrationMessage(event) {
+            if (event.origin !== 'https://oribos.exchange') {
+                return;
+            }
+
+            if (event.data.action !== 'migration') {
+                return;
+            }
+
+            const storage = event.data.storage || {};
+            Object.keys(storage).forEach(key => {
+                localStorage.setItem(key, storage[key]);
+            });
+
+            location.reload();
+        }
+
+        /**
+         * Stuff to run on oribos.exchange for the migration.
+         */
+        function oribosPart() {
+            // TODO: my.abortInit = true;
+            // TODO: redirect to undermine.exchange, keeping the hash.
+        }
+
+        /**
+         * Stuff to run on undermine.exchange for the migration.
+         */
+        function underminePart() {
+            // Quit if we've already attempted migration.
+            if (localStorage.getItem('migrated')) {
+                return;
+            }
+
+            // Mark migration as attempted. Quit if we can't write to localstorage.
+            try {
+                localStorage.setItem('migrated', '1');
+            } catch (e) {
+                console.log('Local storage not available.');
+                return;
+            }
+
+            // Prepare to receive migration messages.
+            window.addEventListener('message', migrationMessage);
+
+            // Open the migration iframe.
+            document.body.appendChild(ce('iframe', {
+                style: {display: 'none'},
+                src: 'https://oribos.exchange/migration.html',
+            }));
+        }
+
+        init();
+    };
+
     //                           //
     // Generic Utility Functions //
     //                           //
@@ -6066,6 +6170,10 @@ new function () {
     async function init() {
         const inMaintenance = !!qs('.main .welcome').dataset.maintenance;
         if (inMaintenance) {
+            return;
+        }
+
+        if (UndermineMigration.abortInit()) {
             return;
         }
 
